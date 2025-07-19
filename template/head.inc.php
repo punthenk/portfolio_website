e8786b590c438490f19cf4883647648bd6e0ab0f
MichielN19 <Michielmitsjol@gmail.com>
2025-07-19 14:18:40 +0200

    Changed navlink on mobile more click space and space between

--- template/head.inc.php
+++ template/head.inc.php
@@ -20,7 +20,7 @@
     <!-- </nav> -->
     <nav class="w-full border-b border-border-custom fixed top-0 bg-background/20 backdrop-blur-sm z-50" id="navbar">
       <div class="px-4 relative mx-auto flex max-w-screen-lg flex-col py-4 sm:flex-row sm:items-center sm:justify-between">
-        <a href="index.php" class="h-10"><img class="h-full" src="img/MN_logo.png" alt="Michiel Nijenhuis logo"></a>
+        <a href="index.php#home" class="h-10"><img class="h-full" src="img/MN_logo.png" alt="Michiel Nijenhuis logo"></a>
         <input class="peer hidden" type="checkbox" id="navbar-open" />
         <label class="absolute right-0 mt-2 mr-3 cursor-pointer text-xl sm:hidden" for="navbar-open">
           <span class="sr-only">Toggle Navigation</span>
@@ -28,10 +28,10 @@
         </label>
         <nav aria-label="Header Navigation" class="peer-checked:block hidden pl-2 py-6 sm:block sm:py-0">
           <ul class="flex flex-col sm:flex-row">
-            <li class=""><a class="font-bold nav-link transition-all duration-200 ease-out px-4" href="index.php#home">HOME</a></li>
-            <li class=""><a class="font-bold nav-link transition-all duration-200 ease-out px-4" href="index.php#about_me">ABOUT ME</a></li>
-            <li class=""><a class="font-bold nav-link transition-all duration-200 ease-out px-4" href="projects.php">PROJECTS</a></li>
-            <li class=""><a class="font-bold nav-link transition-all duration-200 ease-out px-4" href="index.php#contact">CONTACT</a></li>
+            <!-- <li class=""><a class="font-bold nav-link transition-all duration-200 ease-out px-4" href="index.php#home">HOME</a></li> -->
+            <a class="font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link" href="index.php#about_me"><li class="">ABOUT ME</li></a>
+            <a class="font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link" id="project" href="projects.php"><li class="">PROJECTS</li></a>
+            <a class="font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link" href="index.php#contact"><li class="">CONTACT</li></a>
           </ul>
         </nav>
       </div>
