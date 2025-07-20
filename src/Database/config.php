d9cc7cef43de6ff83a77da23acb81ddd4eff0444
MichielN19 <Michielmitsjol@gmail.com>
2025-07-20 14:28:55 +0200

    updated .gitignore and push local config.php

--- src/Database/config.php
+++ src/Database/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'DB_HOST' => 'localhost',
-    'DB_NAME' => 'u804674418_portfolio',
-    'DB_USER' => 'u804674418_punthenk',
-    'DB_PASS' => 'l=V84R>sZoIT',
+    'DB_HOST' => 'db',
+    'DB_NAME' => 'portfolio',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'root',
 ];
