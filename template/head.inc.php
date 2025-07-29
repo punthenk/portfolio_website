373feb9eac46a727a747e4e327f1360e64e4e76c
MichielN19 <Michielmitsjol@gmail.com>
2025-07-29 19:55:51 +0200

    Delelted input.css from head because of the console error and we don't need it

--- template/head.inc.php
+++ template/head.inc.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/input.css">
     <link rel="stylesheet" href="css/output.css">
     <link rel="icon" type="image/x-icon" href="img/icons/favicon.ico">
     <title>Michiel Nijenhuis</title>
