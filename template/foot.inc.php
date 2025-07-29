174e724937a70fa20c50c1bd415fff7b516609a6
MichielN19 <Michielmitsjol@gmail.com>
2025-07-29 21:28:11 +0200

    Updated navbar with animations and working active colors

--- template/foot.inc.php
+++ template/foot.inc.php
@@ -3,8 +3,7 @@
         <a href="https://github.com/punthenk" target="_blank" class="h-6 hover:cursor-pointer"><img class="h-full w-auto opacity-50 hover:opacity-100 transition-all duration-200" src="img/github-icon-2.svg" alt="github logo"></a>
         <p><span>&copy;</span>Michiel Nijenhuis</p>
     </footer>
-    <script src="js/sendMessage.js?ver=10"></script>
-    <script src="js/main.js?ver=11"></script>
-    <script src="js/updateVersion.js?ver=10"></script>
+    <script src="js/sendMessage.js?ver=1"></script>
+    <script src="js/main.js?ver=1"></script>
 </body>
 </html>
--- template/head.inc.php
+++ template/head.inc.php
@@ -10,43 +10,33 @@
 </head>
 
 <body class="font-inter text-white bg-background">
-    <!-- <nav class="bg-background border-b border-border-custom h-[100px] flex flex-row justify-around items-center"> -->
-    <!--     <a href="index.html" class="h-10"><img class="h-full" src="img/MN_logo.png" alt="Michiel Nijenhuis logo"></a> -->
-    <!--     <div class="flex gap-5"> -->
-    <!--         <a href="" class="text-white font-extrabold text-lg">HOME</a>  -->
-    <!--         <a href="index.html#about_me" class="text-white font-extrabold text-lg">ABOUT ME</a>  -->
-    <!--         <a href="" class="text-white font-extrabold text-lg">PROJECTS</a>  -->
-    <!--         <a href="" class="text-white font-extrabold text-lg">CONTACT</a>  -->
-    <!--     </div> -->
-    <!-- </nav> -->
     <nav class="w-full border-b border-border-custom fixed top-0 bg-background/20 backdrop-blur-sm z-50" id="navbar">
-        <div
-            class="px-4 relative mx-auto flex max-w-screen-lg flex-col py-4 sm:flex-row sm:items-center sm:justify-between">
-            <a href="index.php#home" class="h-10"><img class="h-full" src="img/MN_logo.png"
-                    alt="Michiel Nijenhuis logo"></a>
+        <div class="px-4 relative flex max-w-screen-lg mx-auto flex-col py-4 sm:flex-row sm:items-center sm:justify-between">
+            <a href="index.php#home" class="h-10 w-max shrink-0"><img class="h-10 w-auto" src="img/MN_logo.png"
+                alt="Michiel Nijenhuis logo"></a>
             <input class="peer hidden" type="checkbox" id="navbar-open" />
-            <label class="absolute right-0 mt-2 mr-3 cursor-pointer text-xl sm:hidden z-50" for="navbar-open">
+            <label class="absolute right-0 mt-2 mr-4 cursor-pointer text-xl sm:hidden z-50" for="navbar-open">
                 <span class="sr-only">Toggle Navigation</span>
-                <img class="w-5" src="img/HamburgerIcon.png" alt="Hamburgen icon">
+                <img class="w-7" src="img/HamburgerIcon.png" alt="Hamburgen icon">
             </label>
-            <nav aria-label="Header Navigation" class="peer-checked:block hidden pl-2 py-6 sm:block sm:py-0">
-                <ul class="flex flex-col sm:flex-row">
-                    <!-- <li class=""><a class="font-bold nav-link transition-all duration-200 ease-out px-4" href="index.php#home">HOME</a></li> -->
-                    <a class="font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link"
-                        href="index.php#about_me">
-                        <li class="">ABOUT ME</li>
-                    </a>
-                    <a class="font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link"
-                        href="index.php#contact">
-                        <li class="">CONTACT</li>
-                    </a>
-                    <a class="font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link" id="project"
-                        href="projects.php">
-                        <li class="">PROJECTS</li>
-                    </a>
-                </ul>
-            </nav>
+            <div class="transition-max-height duration-500 ease-linear overflow-hidden max-h-0 peer-checked:max-h-96">
+                <nav aria-label="Header Navigation" class="pl-2 py-6 sm:block sm:py-0">
+                    <ul class="flex flex-col sm:flex-row text-xl">
+                        <li class="my-2"><a class="nav-link block w-full font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link"
+                            href="index.php#about_me">ABOUT ME</a></li>
+                        <li class="my-2"><a class="nav-link block w-full font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link"
+                            href="index.php#contact">CONTACT</a></li>
+                        <li class="my-2"><a class="nav-link block w-full font-bold nav-link transition-all duration-200 ease-out px-4 mobile-link"
+                               href="projects.php">PROJECTS</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="hidden sm:flex w-max font-bold text-lg">
+                <a href="index.php#about_me" class="w-max px-4 transition-all duration-200 ease-out nav-link">ABOUT ME</a>
+                <a href="index.php#contact" class="w-max px-4 transition-all duration-200 ease-out nav-link">CONTACT</a>
+                <a href="projects.php" id="project" class="w-max px-4 transition-all duration-200 ease-out nav-link">PROJECTS</a>
+            </div>
         </div>
     </nav>
 
-    <div class="h-[73px]" id="<?php if($_SERVER['PHP_SELF'] === " /projects.php") {echo "project" ;} else {echo "home" ;} ?>"></div>
+    <div class="h-[73px]" id="<?php if($_SERVER['PHP_SELF'] === "/projects.php") {echo "project" ;} else {echo "home" ;} ?>"></div>
